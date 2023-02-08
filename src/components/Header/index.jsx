45ccff641c9348f6a7693c4e406518a8f03fe3a2
Caio Cabral <ocaiocabral@gmail.com>
2023-02-08 15:10:29 -0400

    feat: favicon

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,13 +1,14 @@
 import logo from './img/logo.png';
 import search from './img/search.png';
+import styles from './style/header.module.scss'
 
 export default function Header () {
    return(
-      <header>
+      <header className={styles.header}>
          <a href="#">
             <img src={logo} alt="Logo for the website" />
          </a>
-         <form>
+         <form className={styles.header__searchField}>
             <input type="text" placeholder="O que você procura?"/>
             <img src={search} alt="Magnifier"/>
          </form>
